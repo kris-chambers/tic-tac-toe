2e44c1f3f9c3eeaddcfdfe3ab886940ecdfb643f
Kris Chambers <103455140+kris-chambers@users.noreply.github.com>
2022-12-31 12:58:46 -0500

    Begin play game function

--- javascript.js
+++ javascript.js
@@ -1,14 +1,6 @@
+// Object for actual gameboard
 const gameBoard = (function() {
-  const cellZero = document.querySelector('#zero');
-  const cellOne = document.querySelector('#one');
-  const cellTwo = document.querySelector('#two');
-  const cellThree = document.querySelector('#three');
-  const cellFour = document.querySelector('#four');
-  const cellFive = document.querySelector('#five');
-  const cellSix = document.querySelector('#six');
-  const cellSeven = document.querySelector('#seven');
-  const cellEight = document.querySelector('#eight');
-  const gameBoardSpaces = ["X","O","X","O","X","O","X","O","X"];
+  const gameBoardSpaces = ["X","","","","","","","",""];
   const listOfIDs = ["zero","one","two","three","four","five","six",
   "seven","eight"];
   for (let i = 0; i < gameBoardSpaces.length; i++) {
@@ -24,9 +16,28 @@ const gameBoard = (function() {
   }
 })();
 
+// Creation of players
 const Player = (playerName, playerMarker) => {
   return {playerName, playerMarker};
 };
+const playerOne = Player("one", 'X');
+const playerTwo = Player("two", 'O');
 
-const playerOne = Player(one, 'X');
-const playerTwo = Player(two, 'O');
+// Playing the game
+const playGame = (function() {
+  const cellZero = document.querySelector('#zero');
+  const cellOne = document.querySelector('#one');
+  const cellTwo = document.querySelector('#two');
+  const cellThree = document.querySelector('#three');
+  const cellFour = document.querySelector('#four');
+  const cellFive = document.querySelector('#five');
+  const cellSix = document.querySelector('#six');
+  const cellSeven = document.querySelector('#seven');
+  const cellEight = document.querySelector('#eight');
+  
+  cellZero.addEventListener('click', (event) => {
+    console.log("Test");
+    gameBoard.gameBoardSpaces[0] = "X";
+    
+  });
+})();
