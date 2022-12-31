ad50f746e0cfbc2a460e2c279183cd997e46e0c2
Kris Chambers <103455140+kris-chambers@users.noreply.github.com>
2022-12-31 11:12:09 -0500

    Complete JS rewrite

--- javascript.js
+++ javascript.js
@@ -1,54 +1,28 @@
-const cellZero = document.querySelector('#zero');
-const cellOne = document.querySelector('#one');
-const cellTwo = document.querySelector('#two');
-const cellThree = document.querySelector('#three');
-const cellFour = document.querySelector('#four');
-const cellFive = document.querySelector('#five');
-const cellSix = document.querySelector('#six');
-const cellSeven = document.querySelector('#seven');
-const cellEight = document.querySelector('#eight');
-const gameBoardCells = [cellZero, cellOne, cellTwo, cellThree, cellFour,
-   cellFive, cellSix, cellSeven, cellEight]
-
-// Function that adds marker to gameboard when given player 
-// and which cell to mark
-function addMarker(player, cell) {
-  const div = document.createElement('div');
-  cell.appendChild(div);
-  div.classList.add('gameLetter');
-  const marker = player.playerMarker
-  div.textContent = marker;
-}
-
-// Player creation factory function
-const player = (playerName, playerMarker) => {
-  return {playerName, playerMarker}
-}
-
-// Creation of players One and Two
-const playerOne = player('Player One', 'X');
-const playerTwo = player('Player Two', 'O');
-
-// Function for game board
-const gameBoard = (() => {
-  let gameBoardCells = {
-    cellZero: "",
-    cellOne: "",
-    cellTwo: "",
-    cellThree: "",
-    cellFour: "",
-    cellFive: "",
-    cellSix: "X",
-    cellSeven: "",
-    cellEight: "",
-  }
-  for (const cell in gameBoardCells) {
+const gameBoard = (function() {
+  const cellZero = document.querySelector('#zero');
+  const cellOne = document.querySelector('#one');
+  const cellTwo = document.querySelector('#two');
+  const cellThree = document.querySelector('#three');
+  const cellFour = document.querySelector('#four');
+  const cellFive = document.querySelector('#five');
+  const cellSix = document.querySelector('#six');
+  const cellSeven = document.querySelector('#seven');
+  const cellEight = document.querySelector('#eight');
+  const gameBoardSpaces = ["X","O","X","O","X","O","X","O","X"];
+  for (let i = 0; i < gameBoardSpaces.length; i++) {
     const div = document.createElement('div');
-    cell.appendChild(div);
+    const board = document.querySelector('.gameboard');
+    board.appendChild(div);
+    div.classList.add('cell');
     div.classList.add('gameLetter');
-    const marker = gameBoardCells[cell];
+    const marker = gameBoardSpaces[i];
     div.textContent = marker;
   }
 })();
 
-gameBoard();
+const Player = (playerName, playerMarker) => {
+  return {playerName, playerMarker};
+};
+
+const playerOne = Player(one, 'X');
+const playerTwo = Player(two, 'O');
